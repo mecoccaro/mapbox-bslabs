a761d1e0b285f8130de45d014d445d0c37f1a259
mecoccaro <miguele.coccaro@yahoo.com>
2021-06-23 19:15:45 -0400

    mapbox and geocoder

--- mapbox-angular/src/app/app.component.ts
+++ mapbox-angular/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {environment} from '../environments/environment';
+import * as mapboxgl from 'mapbox-gl'
+// @ts-ignore
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mapbox-angular';
+export class AppComponent implements OnInit{
+
+  ngOnInit() {
+
+    // @ts-ignore
+    mapboxgl.accessToken = environment.mapboxKey;
+
+    var map = new mapboxgl.Map({
+      container: 'map-mapbox',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-122.25948, 37.87221],
+      zoom: 12,
+    });
+
+    var geocoder = new MapboxGeocoder({
+      accessToken: environment.mapboxKey,
+      mapboxgl: mapboxgl,
+      marker: false,
+    });
+
+    map.addControl(geocoder);
+
+  }
+
 }
